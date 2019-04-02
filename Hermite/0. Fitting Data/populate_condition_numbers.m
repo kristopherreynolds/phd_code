0533d6a120911477870cca81843420df39869176
Reynolds <reynokl1@reynokl1-ml1.jhuapl.edu>
2019-04-02 15:00:33 -0400

    moved and deleted files

--- Hermite/0. Fitting Data/populate_condition_numbers.m
+++
@@ -1,8 +0,0 @@
-%Populate Condition Numbers
-
-npts = 2*512;
-Nvec = 300:500;
-Rvec = linspace(25,30,25);
-C = condition_number(Rvec,Nvec,npts);
-figure;
-imagesc(Nvec,Rvec,log(abs(C)))
--- Hermite/herm_coeff.m
+++
@@ -1,4 +0,0 @@
-function out = herm_coeff(n)
-%function cn = herm_coeff(n)
-out = 1./(2.^(n./2).*sqrt(factorial(n).*sqrt(pi)));
-end
