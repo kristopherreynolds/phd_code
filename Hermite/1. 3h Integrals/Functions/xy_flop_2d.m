8f0addea61d224f3133a351a2a79295efa572a71
reynokl1 <reynokl1@reynokl1-ml1.jhuapl.edu>
2019-04-02 16:30:02 -0400

    fixed singularity in large bandlimit for 2d rr prop

--- Hermite/1. 3h Integrals/Functions/xy_flop_2d.m
+++ Hermite/1. 3h Integrals/Functions/xy_flop_2d.m
@@ -19,10 +19,39 @@ cnp1 = herm_coeff(n+1);
 cnm1 = herm_coeff(n-1);
 cn = herm_coeff(n);
 
+
+
 L_p1 = diag([cmp1*cn cm*cnp1]);
 L_m1 = diag([cmm1*cn cm*cnm1]);
+L_m1inv = diag(1./(diag(L_m1)));
 
-h_p1 = L_p1*D_H/(L_m1)*h_m1;
+d11 = D_H(1,1);
+d12 = D_H(1,2);
+d21 = D_H(2,1);
+d22 = D_H(2,2);
+
+a1 = L_p1(1,1);
+a2 = L_p1(2,2);
+b1 = L_m1inv(1,1);
+b2 = L_m1inv(2,2);
+
+Q =  L_p1*D_H*L_m1inv;
+
+Q = [a1*b1*d11,a1*b2*d12;a2*b1*d21,a2*b2*d22];
+
+Q = [cmp1*d11/cmm1,cmp1*cn*d12/(cm*cnm1);cm*cnp1*d21/(cmm1*cn),cnp1*d22/cnm1];
+
+Q(1,1) = d11*2^((m-1)/2-(m+1)/2)*sqrt(1/(m*(m+1)));
+Q(2,2) = d22*2^((n-1)/2-(n+1)/2)*sqrt(1/(n*(n+1)));
+Q(1,2) = 0.5*sqrt(1/(n*(m+1)))*d12;
+Q(2,1) = 0.5*sqrt(1/(m*(n+1)))*d21;
+
+
+h_p1 =Q*h_m1;
+
+if cn < eps
+    %keyboard
+end
 
 %replace nans
 h_p1(isnan(h_p1)) = 0;
